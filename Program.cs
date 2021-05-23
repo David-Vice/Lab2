d923cd8c9a98689f2be460e53a1eeef7a919bc50
Medz_rp99@itstep.edu.az <Medz_rp99@itstep.edu.az>
2021-05-23 19:00:59 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app12
+{
+    public delegate void PropertyEventHandler(object sender, PropertyEventArgs e);
+    public interface IPropertyChanged
+    {
+        event PropertyEventHandler PropertyChanged;
+    }
+
+    public class PropertyEventArgs
+    {
+        public PropertyEventArgs(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+    }
+
+    public class Number : IPropertyChanged
+    {
+        public event PropertyEventHandler PropertyChanged;
+        private int myVar;
+
+        public Number(int myProperty)
+        {
+            MyProperty = myProperty;
+        }
+        public Number()
+        {
+            myVar = 0;
+        }
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set 
+            { 
+                myVar = value;
+                PropertyChanged.Invoke(this, new PropertyEventArgs($"Property has been changed to {value}!"));
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number a = new Number();
+            a.PropertyChanged += SendMessage;
+            a.MyProperty = 5;
+        }
+        static void SendMessage(object sender, PropertyEventArgs e)
+        {
+            Console.WriteLine($"{e.Message}");
+        }
+    }
+}
